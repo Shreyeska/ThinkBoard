82f3ee15ba1abea5bc36a84b456cb05dec564864
Shreyeska <silwalshreyeska@gmail.com>
2025-06-20 19:01:17 +0545

    Added redis rate limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -13,8 +14,12 @@ connectDB();
 //Middleware
 app.use(express.json());
 
+app.use(rateLimiter);
+
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server started on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
