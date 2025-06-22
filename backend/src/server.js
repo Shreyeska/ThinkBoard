c2f7499c8bb87eb433833f920d9d692ebdc76a58
Shreyeska <silwalshreyeska@gmail.com>
2025-06-22 23:10:59 +0545

    Updated cors, homepage and components for ratelimited

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 connectDB();
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 
 app.use(rateLimiter);
