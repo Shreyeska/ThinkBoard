21db6b882e857e5ef0b5d8dab66957c67f1ec213
Shreyeska <silwalshreyeska@gmail.com>
2025-06-26 22:50:11 +0545

    Completed Project

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { useNavigate, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import api from "../network/api";
-import { LoaderIcon } from "lucide-react";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
 
 const NoteDetailPage = () => {
   const [note, setNote] = useState(null);
@@ -26,7 +26,37 @@ const NoteDetailPage = () => {
     };
     fetchNote();
   }, [id]);
-  console.log(note);
+
+  const handleDelete = async () => {
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+    try {
+      await api.delete(`/notes/${id}`);
+      toast.success("Note Deleted Successfully!");
+      navigate("/");
+    } catch (error) {
+      console.error("Error deleting note", error);
+      toast.error("Failed to delete this note.");
+    }
+  };
+  const handleSave = async () => {
+    if (!note.title.trim() || !note.description.trim()) {
+      toast.error("Please add title or description");
+      return;
+    }
+    setSaving(true);
+
+    try {
+      await api.put(`/notes/${id}`, note);
+      toast.success("Note updated successfully.");
+      navigate("/");
+    } catch (error) {
+      console.log("Error saving the note", error);
+      toast.error("Failed to update note");
+    } finally {
+      setSaving(false);
+    }
+  };
+
   if (loading) {
     return (
       <div className="min-h-screen bg—base—200 flex items—center justify—center">
@@ -36,7 +66,60 @@ const NoteDetailPage = () => {
   }
   return (
     <div className="min-h-screen bg-base-200">
-      <h1>Note Page</h1>
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6 ">
+            <Link to="/" className="btn btn-ghost">
+              <ArrowLeftIcon className="h-5 w-5" />
+              Back to notes
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="btn btn-error btn-outline"
+            >
+              <Trash2Icon className="h-5 w-5" /> Delete Note
+            </button>
+          </div>
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text"> Title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Note Title"
+                  className="input input-bordered"
+                  value={note.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                />
+              </div>
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Description</span>
+                </label>
+                <textarea
+                  placeholder="Write your note here..."
+                  className="textarea textarea-bordered h-32"
+                  value={note.description}
+                  onChange={(e) =>
+                    setNote({ ...note, description: e.target.value })
+                  }
+                />
+              </div>
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-primary "
+                  disabled={saving}
+                  onClick={handleSave}
+                >
+                  {saving ? "Saving... " : "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
